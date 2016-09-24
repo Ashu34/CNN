27779e86efdb2144acad3f76c42b24d5c4549b54
Rishabh Srivastava <f2013674@goa.bits-pilani.ac.in>
2016-09-25 01:13:21 +0530

    Update convolution.py

--- convolution.py
+++ convolution.py
@@ -1,18 +1,20 @@
 import numpy as np
 def convolution(X,F,S,P,W,B):
-	X=np.pad(X,[(P,P),(P,P),(0,0)],'constant',constant_values=0)
+	''' Convolution operation on square 3D signals, X: Signal, F:Filter, S: Stride, P padding to get desired output size,
+	W:Weight matrix, B: Bias value'''
+	X=np.pad(X,[(P,P),(P,P),(0,0)],'constant',constant_values=0) # pad edges with zeros 
 	a=X.shape[0]
 	b=F[0]
-	c=((a-b)/S)+1#output Dimensions
+	c=((a-b)/S)+1 #output Dimensions
 	v=a*1.0
 	n=b
 	m=((v-n)/S)+1
 	if((m).is_integer() !=True or X.shape[2] != F[2]): 
 		print "Invalid Inputs in Conv Layer"
-		return 0,0
+		return 0,0 # return Zero and raise error : incompatible convolution operands
 	else:
-		out = np.repeat((np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1)+ np.repeat(xrange(0,b,1),b).reshape(1,-1)),c,axis=0)#row indicies
-		out1=np.tile(np.repeat(np.repeat(xrange(0,b,1),1).reshape(1,-1)+np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1),b,axis=0),(c,1))#column indicies
+		out = np.repeat((np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1)+ np.repeat(xrange(0,b,1),b).reshape(1,-1)),c,axis=0) #obtain row indicies of the matrix
+		out1=np.tile(np.repeat(np.repeat(xrange(0,b,1),1).reshape(1,-1)+np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1),b,axis=0),(c,1))#Obtain column indicies of the matrix
 		im2col=X[out.flatten(),out1.flatten()].reshape(1,c*c*b*b*X.shape[2]).reshape(b*b*X.shape[2],c*c,order="F")
 		output_volume=np.dot(W,im2col).reshape(c,c,W.shape[0])+B
 		return output_volume,im2col
