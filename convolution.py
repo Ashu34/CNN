c685faca5637997c7b421a6719873e1e8b348a54
Rishabh Srivastava <f2013674@goa.bits-pilani.ac.in>
2016-10-03 03:28:21 +0530

    Fix col2im

--- convolution.py
+++ convolution.py
@@ -1,5 +1,6 @@
 import numpy as np
 def convolution(X,F,S,P,W,B):
+	"""X: Input signal,F: Filter dimensions, S: Stride, P: padding, W: Netwrok Weights, B: Bias"""
 	X=np.pad(X,[(P,P),(P,P),(0,0)],'constant',constant_values=0)
 	a=X.shape[0]
 	b=F[0]
@@ -7,7 +8,7 @@ def convolution(X,F,S,P,W,B):
 	v=a*1.0
 	n=b
 	m=((v-n)/S)+1
-	if((m).is_integer() !=True or X.shape[2] != F[2]): 
+	if((m).is_integer() !=True or X.shape[2] != F[2]): #Return incompatible for convolution operation
 		print "Invalid Inputs in Conv Layer"
 		return 0,0
 	else:
@@ -16,8 +17,7 @@ def convolution(X,F,S,P,W,B):
 		im2col=X[out.flatten(),out1.flatten()].reshape(1,c*c*b*b*X.shape[2]).reshape(b*b*X.shape[2],c*c,order="F")
 		output_volume=np.dot(W,im2col).reshape(c,c,W.shape[0])+B
 		return output_volume,im2col
-		
-		
+				
 def maxpool(X,F,S):
 	a=X.shape[0]
 	b=F[0]
@@ -38,16 +38,13 @@ def maxpool(X,F,S):
  	
  	return out_vol.reshape(c,c,X.shape[2])
 
-
-
-
 def col2im(im2col,P,F,X,S):
 	X[0]=X[0]+2*P
 	X[1]=X[1]+2*P
 	a=X[0]
 	b=F[0]
 	c=((a-b)/S)+1#output Dimensions
-	im2col=im2col.reshape(c*c*b*b*X[2],1,order="F").reshape(c*c*b*b,X[2],order="C")
+	im2col=im2col.reshape(1,c*c*b*b*X[2],order="F").reshape(c*c*b*b,X[2],order="C")
 	out = np.repeat((np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1)+ np.repeat(xrange(0,b,1),b).reshape(1,-1)),c,axis=0)#row indicies
 	out1=np.tile(np.repeat(np.repeat(xrange(0,b,1),1).reshape(1,-1)+np.repeat(xrange(0,a-b+1,S),1).reshape(-1,1),b,axis=0),(c,1))#column indicies
 	Xa=np.empty(((X[0],X[1],X[2])))
